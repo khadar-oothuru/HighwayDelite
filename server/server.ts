cd6436a7e474c4fc34a1d20f20ea39d51c4399f4
khadar-oothuru <khadaroothuru@gmail.com>
2025-07-28 11:26:23 +0530

    script added  in backend

--- server/server.ts
+++ server/server.ts
@@ -12,7 +12,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://highway-delite-iota.vercel.app/',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
